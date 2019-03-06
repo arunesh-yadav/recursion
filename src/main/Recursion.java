fd92d9b50f4a286e435d9a800fecc8c7c1359860
arunesh-yadav <arunesh.kumar_ccv17@gla.ac.in>
2019-03-06 10:40:14 +0530

    call countdown method in main class

--- src/main/Recursion.java
+++ src/main/Recursion.java
@@ -1,5 +1,14 @@
 package main;
 
 public class Recursion {
+     static void countDown(int number){
+        System.out.println(number);
+        --number;
+        countDown(number);
+    }
 
+    public static void main(String[] args) {
+       countDown(4);
+
+    }
 }
